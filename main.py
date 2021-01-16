5684281fa8d06c5e2ea01c6496b24301fa578fb5
RainerMauersberger23 <829532@mystma.org>
2021-01-16 04:35:48 +0000

    Unit 2 Test

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-#WRITE YOUR CODE IN THIS FILE
+def isDozen(d):
+    if d % 2 == 0:
+        return True
+    else:
+        return False
+
+print(isDozen(12))
+
+
+
+
+def waterState(state):
+    
+    if state == "20":
+        return "solid"
+
+    elif state == "10":
+        return "liquid"
+
+    else:
+        return "gas"
+
+print(waterState(20))
+
+
+def stopLight(color):
+    if color == "green":
+        return "Go"
+    elif color == "yellow":
+        return "Slow"
+    
+    else:
+        return "Stop"
+
+print(stopLight("green"))
+
